0efaa769ed9d690e84ae417fc1a154de434b7bee
yaezzin <yaezzin@gmail.com>
2025-03-10 16:32:46 +0900

    Docker CI

---
+++ src/main/java/com/example/dockerci/DockerCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerCiApplication.class, args);
+    }
+
+}
