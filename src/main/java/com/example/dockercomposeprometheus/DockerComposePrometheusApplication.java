ff99a50a8788059a8ed606f6c947ce9b356d41b3
yaezzin <yaezzin@gmail.com>
2025-03-31 16:19:37 +0900

    Docker Prometheus Grafana

---
+++ src/main/java/com/example/dockercomposeprometheus/DockerComposePrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockercomposeprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposePrometheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerComposePrometheusApplication.class, args);
+    }
+
+}
