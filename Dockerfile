a9b6b6d37f31ee83c741eb1dd4cfd98d41c66f24
yaezzin <yaezzin@gmail.com>
2025-04-01 15:36:25 +0900

    Release Tag 자동화

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:17-jdk-slim
+
+WORKDIR /app
+
+COPY build/libs/release-tag-0.0.1-SNAPSHOT.jar app.jar
+
+CMD ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/example/releasetag/ReleaseTagApplication.java
@@ -0,0 +1,13 @@
+package com.example.releasetag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseTagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleaseTagApplication.class, args);
+    }
+
+}
